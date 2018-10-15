e11b874d9f5a2bd661cb27f812c79e476178f850
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-10-15 16:27:48 +0500

    Добавил поле ввода даты

--- client/src/components/EditDialog.tsx
+++ client/src/components/EditDialog.tsx
@@ -1,5 +1,7 @@
-import { Form, Icon, Input, Modal } from 'antd';
+import { DatePicker, Form, Input, Modal } from 'antd';
+import * as moment from 'moment';
 import * as React from 'react';
+import './EditDialog.css';
 
 const FormItem = Form.Item;
 interface IMovie {
@@ -31,39 +33,41 @@ class EditDialog extends React.Component<IEditDialogProps, IEditDialogState> {
 
     this.handleTitleChange = this.handleTitleChange.bind(this);
     this.onHandleCancel = this.onHandleCancel.bind(this);
+    this.handleReleaseDateChange = this.handleReleaseDateChange.bind(this);
   }
 
   public render() {
     const { visible, selectedMovie } = this.props;
     return (
       <Modal
+        className="EditDialog"
         title="Редактировать информацию о фильме"
         visible={visible}
         onCancel={this.onHandleCancel}
       >
-        <section className="serverData">
-          <h2>Данные на сервере</h2>
-          <div>Название фильма: {selectedMovie.title}</div>
-          <div>Дата релиза: {selectedMovie.releaseDate}</div>
-          <div>Год выхода: {selectedMovie.year}</div>
-          <div>Продолжительность: {selectedMovie.duration}</div>
-        </section>
-        <Form className="inputData">
-          <FormItem>
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="text"
-              placeholder="Название фильма"
-              value={this.state.editedMovie.title}
-              onChange={this.handleTitleChange}
-            />
-          </FormItem>
-
-          {/* <Input
-            value={this.state.editedMovie.title}
-            onChange={this.handleTitleChange}
-          /> */}
-        </Form>
+        <div className="EditDialog--content">
+          <section className="serverData">
+            <h2>Данные на сервере</h2>
+            <div>Название фильма: {selectedMovie.title}</div>
+            <div>Дата релиза: {selectedMovie.releaseDate}</div>
+            <div>Год выхода: {selectedMovie.year}</div>
+            <div>Продолжительность: {selectedMovie.duration}</div>
+          </section>
+          <Form className="inputData">
+            <h2>Изменить данные</h2>
+            <FormItem>
+              <Input
+                type="text"
+                placeholder="Название фильма"
+                value={this.state.editedMovie.title}
+                onChange={this.handleTitleChange}
+              />
+            </FormItem>
+            <FormItem>
+              <DatePicker onChange={this.handleReleaseDateChange} />
+            </FormItem>
+          </Form>
+        </div>
       </Modal>
     );
   }
@@ -80,6 +84,13 @@ class EditDialog extends React.Component<IEditDialogProps, IEditDialogState> {
     this.setState({
       editedMovie: {}
     });
+  }
+
+  private handleReleaseDateChange(e: any) {
+    const releaseDate = moment(e).format('YYYY-MM-DD');
+    this.setState(state => ({
+      editedMovie: { ...state.editedMovie, releaseDate }
+    }));
   }
 }
 export default EditDialog;
